ec2b9009ae3145c985bb99ccd3d11e0fe0e80aa2
testhub-Lester <lesteracemariano@gmail.com>
2023-11-12 17:18:33 +0800

    Commit 02

--- Js/index.js
+++ Js/index.js
@@ -1,3 +1,23 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.getElementById("menu-btn");
+const closeBtn = document.getElementById("close-btn");
+
+const darkMode = document.querySelector(".dark-mode");
+
+menuBtn.addEventListener("click", () => {
+  sideMenu.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+  sideMenu.style.display = "none";
+});
+
+darkMode.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode-variables");
+  darkMode.querySelector("span:nth-child(1)").classList.toggle("active");
+  darkMode.querySelector("span:nth-child(2)").classList.toggle("active");
+});
+
 Records.forEach((order) => {
   const tr = document.createElement("tr");
   const trContent = `
@@ -5,7 +25,7 @@ Records.forEach((order) => {
         <td>${order.patientTreatment}</td>
         <td>${order.patientStatus}</td>
         <td>${order.time_and_date}</td>
-        <td class="primary">Details</td>
+        <td class="primary"> <a href="#">Details </a></td>
     `;
   tr.innerHTML = trContent;
   document.querySelector("table tbody").appendChild(tr);
